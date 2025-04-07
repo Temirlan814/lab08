154c6c34d46a30050ea2bfcc61b880c7fb40d4d8
Temirlan814 <t.temirlan.2004@mail.ru>
2025-04-08 01:19:40 +0500

    add kind serivce?

--- app/src/main/java/com/example/lab08/MainActivity.kt
+++ app/src/main/java/com/example/lab08/MainActivity.kt
@@ -1,20 +1,50 @@
 package com.example.lab08
 
-import android.os.Bundle
-import androidx.activity.enableEdgeToEdge
+import android.content.BroadcastReceiver
+import android.content.Context
+import android.content.Intent
+import android.content.IntentFilter
 import androidx.appcompat.app.AppCompatActivity
-import androidx.core.view.ViewCompat
-import androidx.core.view.WindowInsetsCompat
+import android.os.Bundle
+import android.view.View
+import android.widget.EditText
 
 class MainActivity : AppCompatActivity() {
+    private lateinit var randomCharacterEditText: EditText
+    private lateinit var serviceIntent: Intent
+    private val broadcastReceiver = object : BroadcastReceiver() {
+        override fun onReceive(context: Context?, intent: Intent?) {
+            intent?.let {
+                val data = it.getCharExtra("randomCharacter", '?')
+                randomCharacterEditText.setText(data.toString())
+            }
+        }
+    }
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        enableEdgeToEdge()
         setContentView(R.layout.activity_main)
-        ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main)) { v, insets ->
-            val systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars())
-            v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom)
-            insets
+        randomCharacterEditText = findViewById(R.id.editText_randomCharacter)
+        serviceIntent = Intent(applicationContext, RandomCharacterService::class.java)
+    }
+
+    fun onClick(view: View) {
+        when (view.id) {
+            R.id.button_start -> startService(serviceIntent)
+            R.id.button_end -> {
+                stopService(serviceIntent)
+                randomCharacterEditText.text.clear()
+            }
         }
+    }
+
+    override fun onStart() {
+        super.onStart()
+        registerReceiver(broadcastReceiver, IntentFilter("my.custom.action.tag.lab6"))
+    }
+
+    override fun onStop() {
+        super.onStop()
+        unregisterReceiver(broadcastReceiver)
     }
 }
